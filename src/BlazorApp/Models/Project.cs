be47016299c3827bb16b1db9a252ae17888233dd
shalant <doug.rosenberg@gmail.com>
2025-11-24 13:24:07 -0600

    added images, carousel, experience section

--- src/BlazorApp/Models/Project.cs
+++ src/BlazorApp/Models/Project.cs
@@ -5,4 +5,6 @@ public class Project
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public string Url { get; set; } = string.Empty;
+    public string Icons { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
 }
