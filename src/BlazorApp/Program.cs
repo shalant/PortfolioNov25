e7ee329a191ff2859ff8067a12c312aea775dae3
shalant <doug.rosenberg@gmail.com>
2025-11-24 10:26:34 -0600

    added mudblazor, carousel

--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -1,8 +1,10 @@
 using BlazorApp;
 using BlazorApp.Services;
+using MudBlazor.Services;
 
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,4 +13,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<HeroImageService>();
 
+builder.Services.AddMudServices();
+
 await builder.Build().RunAsync();
+
