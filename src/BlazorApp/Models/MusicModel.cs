1f6c47ca92132d1fa236b0619e25e51d7c109a9e
shalant <doug.rosenberg@gmail.com>
2025-11-27 11:06:51 -0600

    added music section

---
+++ src/BlazorApp/Models/MusicModel.cs
@@ -0,0 +1,9 @@
+namespace BlazorApp.Models
+{
+    public class MusicModel
+    {
+        public string Paragraph1 { get; set; } = string.Empty;
+        public string Paragraph2 { get; set; } = string.Empty;
+        public string Paragraph3 { get; set; } = string.Empty;
+    }
+}
